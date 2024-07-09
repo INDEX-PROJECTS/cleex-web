33d4ced8cade0c6fde0a7c71ba6aa3f2b48ca85e
Frontend-4-INDEX <gurugruppp4@gmail.com>
2024-07-09 09:20:46 +0500

    feat(auth): add reset password

--- src/features/auth/ui/CompleteRegistration/CompleteRegistration.tsx
+++ src/features/auth/ui/CompleteRegistration/CompleteRegistration.tsx
@@ -1,14 +1,14 @@
-import { memo, useState } from "react";
-import clsx from "clsx";
-import { CSSTransition } from "react-transition-group";
-import { Button, ThemeButton } from "@/shared/ui/Button/Button";
-import { HStack, VStack } from "@/shared/ui/Stack";
-import { Input } from "@/shared/ui/Input/Input";
-import { Text, TextVariant } from "@/shared/ui/Text/Text";
-import { PasswordStrength } from "@/shared/ui/PasswordStrength/PasswordStrength";
-import { Checkbox } from "@/shared/ui/Checkbox/Checkbox";
-import styles from "./CompleteRegistration.module.scss";
-import type { ChangeEvent } from "react";
+import { memo, useState } from 'react';
+import clsx from 'clsx';
+import { CSSTransition } from 'react-transition-group';
+import type { ChangeEvent } from 'react';
+import { Button, ThemeButton } from '@/shared/ui/Button/Button';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Input } from '@/shared/ui/Input/Input';
+import { Text, TextVariant } from '@/shared/ui/Text/Text';
+import { PasswordStrength } from '@/shared/ui/PasswordStrength/PasswordStrength';
+import { Checkbox } from '@/shared/ui/Checkbox/Checkbox';
+import styles from './CompleteRegistration.module.scss';
 
 interface CompleteRegistrationProps {
   className?: string;
@@ -16,85 +16,84 @@ interface CompleteRegistrationProps {
 }
 
 export const CompleteRegistration = memo((props: CompleteRegistrationProps) => {
-  const { className, handleChangeStep } = props;
+    const { className, handleChangeStep } = props;
 
-  const [meter, setMeter] = useState(false);
-  const [password, setPassword] = useState("");
+    const [meter, setMeter] = useState(false);
+    const [password, setPassword] = useState('');
 
-  const [check, setCheck] = useState(false);
+    const [check, setCheck] = useState(false);
 
-  const onChangeNewPassword = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.currentTarget.value);
-  };
+    const onChangeNewPassword = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.currentTarget.value);
+    };
 
-  return (
-    <VStack
-      max={true}
-      justify="between"
-      className={clsx(styles.CompleteRegistration, {}, [className])}>
-      <VStack
-        max={true}
-        gap="24">
-        <Text
-          gap="0"
-          text="Регистрация"
-          textPrimary={true}
-          variant={TextVariant.TITLE}
-        />
-
+    return (
         <VStack
-          max={true}
-          gap="16">
-          <Input placeholder="Имя пользователя" />
+            max
+            justify="between"
+            className={clsx(styles.CompleteRegistration, {}, [className])}
+        >
+            <VStack
+                max
+                gap="24"
+            >
+                <Text
+                    gap="0"
+                    text="Регистрация"
+                    textPrimary
+                    variant={TextVariant.TITLE}
+                />
 
-          <HStack
-            max={true}
-            className={styles.passwordWrapper}>
-            <Input
-              placeholder="Пароль"
-              isPassword={true}
-              value={password}
-              onBlur={() => {
-                return setMeter(false);
-              }}
-              onFocus={() => {
-                return setMeter(true);
-              }}
-              onChange={onChangeNewPassword}
-            />
-            <CSSTransition
-              in={meter}
-              timeout={300}
-              unmountOnExit={true}
-              classNames="slide-animation">
-              <PasswordStrength
-                password={password || ""}
-                className={styles.passwordPosition}
-              />
-            </CSSTransition>
-          </HStack>
+                <VStack
+                    max
+                    gap="16"
+                >
+                    <Input placeholder="Имя пользователя" />
+
+                    <HStack
+                        max
+                        className={styles.passwordWrapper}
+                    >
+                        <Input
+                            placeholder="Пароль"
+                            isPassword
+                            value={password}
+                            onBlur={() => setMeter(false)}
+                            onFocus={() => setMeter(true)}
+                            onChange={onChangeNewPassword}
+                        />
+                        <CSSTransition
+                            in={meter}
+                            timeout={300}
+                            unmountOnExit
+                            classNames="slide-animation"
+                        >
+                            <PasswordStrength
+                                password={password || ''}
+                                className={styles.passwordPosition}
+                            />
+                        </CSSTransition>
+                    </HStack>
+                </VStack>
+            </VStack>
+            <VStack
+                max
+                gap="16"
+            >
+                <Checkbox
+                    label="Я согласен(-на) с лицензионным соглашением"
+                    checked={check}
+                    id="registrationCheck"
+                    onToggle={() => setCheck(!check)}
+                />
+                <Button
+                    onClick={() => handleChangeStep(0)}
+                    fullWidth
+                    theme={ThemeButton.DEFAULT}
+                >
+                    Зарегистрироваться
+                </Button>
+            </VStack>
         </VStack>
-      </VStack>
-      <VStack
-        max={true}
-        gap="16">
-        <Checkbox
-          label="Я согласен(-на) с лицензионным соглашением"
-          checked={check}
-          id="registrationCheck"
-          onToggle={() => {
-            return setCheck(!check);
-          }}
-        />
-        <Button
-          onClick={() => {
-            return handleChangeStep(0);
-          }}
-          fullWidth={true}
-          theme={ThemeButton.DEFAULT}>
-          Зарегистрироваться
-        </Button>
-      </VStack>
-    </VStack>
-  );
+    );
 });
--- src/features/auth/ui/ResetPasswordStep/ResetPasswordStep.tsx
+++ src/features/auth/ui/ResetPasswordStep/ResetPasswordStep.tsx
@@ -1,8 +1,21 @@
-import { memo } from 'react';
+import {
+    ChangeEvent, memo, useCallback, useState,
+} from 'react';
 import clsx from 'clsx';
+import { CSSTransition } from 'react-transition-group';
 import { Button, ThemeButton } from '@/shared/ui/Button/Button';
-import { VStack } from '@/shared/ui/Stack';
+import { HStack, VStack } from '@/shared/ui/Stack';
 import styles from './ResetPasswordStep.module.scss';
+import { Text, TextVariant } from '@/shared/ui/Text/Text';
+import { Input } from '@/shared/ui/Input/Input';
+import { PasswordStrength } from '@/shared/ui/PasswordStrength/PasswordStrength';
+import { useAppDispatch, useAppSelector } from '@/app/providers/StoreProvider/config/hooks';
+import {
+    getLoginIsLoading,
+    getLoginRepeatResetPassword,
+    getLoginResetPassword,
+} from '../../model/selectors/getLoginData';
+import { loginActions } from '../../model/slice/loginSlice';
 
 interface ResetPasswordStepProps {
   className?: string;
@@ -11,12 +24,81 @@ interface ResetPasswordStepProps {
 
 export const ResetPasswordStep = memo((props: ResetPasswordStepProps) => {
     const { className, handleChangeStep } = props;
+
+    const dispatch = useAppDispatch();
+
+    const [meter, setMeter] = useState(false);
+
+    const resetPassword = useAppSelector(getLoginResetPassword);
+    const repeatResetPassword = useAppSelector(getLoginRepeatResetPassword);
+    const isLoading = useAppSelector(getLoginIsLoading);
+
+    const onChangeResetPassword = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => {
+            dispatch(loginActions.setResetPassword(event.currentTarget.value));
+        },
+        [dispatch],
+    );
+
+    const onChangeRepeatResetPassword = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => {
+            dispatch(loginActions.setRepeatResetPassword(event.currentTarget.value));
+        },
+        [dispatch],
+    );
     return (
         <VStack
             max
+            justify="between"
             className={clsx(styles.ResetPasswordStep, {}, [className])}
         >
+
+            <VStack max gap="24">
+                <Text
+                    gap="0"
+                    text="Регистрация"
+                    textPrimary
+                    variant={TextVariant.TITLE}
+                />
+
+                <VStack max gap="16">
+
+                    <HStack
+                        max
+                        className={styles.passwordWrapper}
+                    >
+                        <Input
+                            placeholder="Введите новый пароль"
+                            isPassword
+                            value={resetPassword}
+                            onBlur={() => setMeter(false)}
+                            onFocus={() => setMeter(true)}
+                            onChange={onChangeResetPassword}
+                        />
+                        <CSSTransition
+                            in={meter}
+                            timeout={300}
+                            unmountOnExit
+                            classNames="slide-animation"
+                        >
+                            <PasswordStrength
+                                password={resetPassword || ''}
+                                className={styles.passwordPosition}
+                            />
+                        </CSSTransition>
+                    </HStack>
+
+                    <Input
+                        placeholder="Повторите пароль"
+                        isPassword
+                        value={repeatResetPassword}
+                        onChange={onChangeRepeatResetPassword}
+                    />
+
+                </VStack>
+            </VStack>
             <Button
+                fullWidth
                 onClick={() => handleChangeStep(2)}
                 theme={ThemeButton.DEFAULT}
             >
